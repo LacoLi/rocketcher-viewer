ccd1e95478d8b673b11e7b101f53bbce481a1877
lacolico <52482311+lacolico@users.noreply.github.com>
2022-09-02 00:57:09 +0900

    귀여움 몇스푼 추가

--- packages/web-server/src/pages/Main/Main.tsx
+++ packages/web-server/src/pages/Main/Main.tsx
@@ -9,17 +9,22 @@
 import React from 'react';
 import axios from 'axios';
 import { Button } from 'antd';
+import Eevee from 'component/Eevee';
+import PokemonFriends from 'component/PokemonFriends';
+import MysteriousGarden from 'component/MysteriousGarden';
+import Pikaboo from 'component/Pikaboo';
+import PumpkinPikachu from 'component/PumpkinPikachu';
 
 type InfoType = {
   result: 'ok' | 'fail';
   data?: object;
 };
 
-interface MainProps {}
+interface MainProps { }
 function Main(props: MainProps) {
   /* ――――――――――――――― Variable ――――――――――――――― */
   /* ===== Props ===== */
-  const {} = props;
+  const { } = props;
   /* ===== State ===== */
   const [username, setUsername] = React.useState<string>('LacoLico');
   const [info, setInfo] = React.useState<InfoType>();
@@ -44,13 +49,19 @@ function Main(props: MainProps) {
 
   /* ―――――――――――――――― Return ―――――――――――――――― */
   return (
+    // <div data-page="main" className='mint-grad-bg'>
     <div data-page="main">
       <em>{info?.result}</em>
+      {/* <Eevee />
+      <PokemonFriends />
+      <MysteriousGarden />
+      <PumpkinPikachu /> */}
+      <Pikaboo />
       <Button onClick={() => getInfo()}>클릭</Button>
     </div>
   );
 }
 
-namespace Main {}
+namespace Main { }
 
 export default Main;
